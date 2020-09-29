fbc103bfafa019dc6477fe22d873610d127b8bae
Hao Shu <shuhao716@outlook.com>
2020-09-28 22:56:06 -0400

    Update constant.py

--- basic/constant.py
+++ basic/constant.py
@@ -1,7 +1,7 @@
 import os
 import logging
 
-ROOT_PATH=os.path.join('/usr/local/extstore01/gengyi/', 'VisualSearch')
+ROOT_PATH=os.path.join('/usr/local/data01/haoshu/', 'VisualSearch')
 
 logger = logging.getLogger(__file__)
 logging.basicConfig(
