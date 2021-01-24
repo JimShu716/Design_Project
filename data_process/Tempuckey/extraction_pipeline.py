7f03dd4b2535ac9473f83c092fb66d86c622ab8a
Han Zhou <44248469+albat3ross@users.noreply.github.com>
2021-01-24 01:09:21 -0500

    Update extraction_pipeline.py

--- data_process/Tempuckey/extraction_pipeline.py
+++ data_process/Tempuckey/extraction_pipeline.py
@@ -17,26 +17,26 @@ CAPTION_SOURCE_PATH = '.\\captions\\'
 VID_1 = '1_TRIPPING_2017-11-28-fla-nyr-home_00_44_55.826000_to_00_45_06.437000.mp4'
 
 class ExtractionPipeline():
-    def __init__(self):
-        self.caption_dict = []
+    def __init__(self, extracted_fps = 10, frame_per_package = 15):
+        self.caption_list = []
         self.video_list = []
-        self.extracted_fps = 10
-        self.frame_per_package = 15
+        self.extracted_fps = extracted_fps
+        self.frame_per_package = frame_per_package
         
         dirpath, dirnames, files = next(os.walk(CAPTION_SOURCE_PATH))
-        self.caption_dict = [file for file in files]
+        self.caption_list = [file for file in files]
 
         dirpath, dirnames, files = next(os.walk(VIDEO_SOURCE_PATH))
         self.video_list = [file for file in files]
         
-        
+        for p in SAVE_PATH, TEST_PATH, VIDEO_SOURCE_PATH, CAPTION_SOURCE_PATH:
+            if not os.path.exists(p):
+                os.mkdir(p)       
     
     def read(self,num_videos = 10, save = True):
         #if num_videos == -1:
         for i in range(num_videos):
             self.read_once(self.video_list[i],save)
-            
-    
     
     def read_once(self,video_name, save = True):
         video_info = self.process_video_name(video_name)
@@ -52,13 +52,13 @@ class ExtractionPipeline():
             'captions':captions,
             'feature':feature,
         }
-        
-        file_pickle = pickle.dumps(file)
-        file_name = video_info['video_name'][:-4]+'.bin'
-        filepath = os.path.join(SAVE_PATH,file_name)
-        f = open(filepath, 'wb')
-        f.write(file_pickle)
-        f.close()
+        if save:
+            file_pickle = pickle.dumps(file)
+            file_name = video_info['video_name'][:-4]+'.bin'
+            filepath = os.path.join(SAVE_PATH,file_name)
+            f = open(filepath, 'wb')
+            f.write(file_pickle)
+            f.close()
         return file
     
     def read_from_saved_binary_file(self, file_name):
@@ -74,10 +74,9 @@ class ExtractionPipeline():
     def frame_to_feature(self, frames):
         # TODO: put code here to do feature embedding extraction
         feature = frames
-        
+        # ===========================
         
         return feature
-    
     
     def process_video_name(self,video_name):
         vlist = video_name.split("_",3)
@@ -97,23 +96,20 @@ class ExtractionPipeline():
             'end_frame':end_frame,
             }
         return info
-    
-    
+       
     def retrieve_captions(self,video_info, save = False):
         lines = []
-        for file in self.caption_dict:
+        for file in self.caption_list:
             if file.split(".")[0] == video_info['video_info']:
                 filepath = os.path.join(CAPTION_SOURCE_PATH,file)
                 fp = open( filepath, "rb")
-                lines = fp.readlines()
-                lines = [line.decode('utf8') for line in lines]
+                lines = [line.decode('utf8') for line in fp.readlines()]
                 fp.close()
         # TODO: fix line format
         
         return lines
-    
-    
-    def retrieve_video_frames(self,video_info, save = False):
+     
+    def retrieve_frames(self,video_info, save = False):
         frames = {}
         video_path = os.path.join(VIDEO_SOURCE_PATH, video_info['video_name'])
         vid_cap = cv2.VideoCapture(video_path)
@@ -150,7 +146,7 @@ class ExtractionPipeline():
 if __name__ == '__main__':
     
     pipe = ExtractionPipeline()
-    pipe.read()
+    #pipe.read()
     #file = pipe.read_once(VID_1)
     #file_2 = pipe.read_from_saved_binary_file(VID_1)
         
