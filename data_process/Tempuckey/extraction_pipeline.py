a24ecb38a20656e722b8b7cb3bf86ceb9b66340e
Han Zhou <44248469+albat3ross@users.noreply.github.com>
2021-01-24 20:52:42 -0500

    now we can find event time

--- data_process/Tempuckey/extraction_pipeline.py
+++ data_process/Tempuckey/extraction_pipeline.py
@@ -9,13 +9,20 @@ import pickle
 import os
 import cv2
 import srt
+import pandas as pd
+import math
+
+
 
 SAVE_PATH = '.\\feature\\'
 TEST_PATH = '.\\test\\'
 VIDEO_SOURCE_PATH = '.\\videos\\'
 CAPTION_SOURCE_PATH = '.\\captions\\'
+LABEL_PATH = '.\\tempuckey_groundtruth_splits_videoinfo_20201026.csv'
 
 VID_1 = '1_TRIPPING_2017-11-28-fla-nyr-home_00_44_55.826000_to_00_45_06.437000.mp4'
+VID_10 = '10_TRIPPING_2017-11-07-vgk-mtl-home_00_42_14.766000_to_00_42_24.142000.mp4'
+
 
 class ExtractionPipeline():
     def __init__(self, num_video = 10, extracted_fps = 10, frame_per_package = 15, suppress_log = True):
@@ -42,18 +49,22 @@ class ExtractionPipeline():
         dirpath, dirnames, files = next(os.walk(VIDEO_SOURCE_PATH))
         self.video_list = [file for file in files]
         
-        
+        # init label lookup list
+        df = pd.read_csv(LABEL_PATH)
+        df = df[df['action']=='tripping']
+        self.label = df
         
         # if number of video to process is too large or less than 0, read all videos
         if self.num_video > len(self.video_list) or self.num_video < 0:
             self.num_video = len(self.video_list)
    
         self.log(f"Initializing the environment...")
-        self.log(f"Read files from \t\t{VIDEO_SOURCE_PATH}")
-        self.log(f"Caption file sourced from \t{CAPTION_SOURCE_PATH}")
-        self.log(f"Save file to \t\t\t{SAVE_PATH}\n")
-        self.log(f"Found {len(self.video_list)} videos and {len(self.caption_list)} subtitle files from environment.")
-
+        self.log(f"Video file source from \t\t: {VIDEO_SOURCE_PATH}")
+        self.log(f"Caption file source from \t: {CAPTION_SOURCE_PATH}")
+        self.log(f"Label file source from \t\t: {LABEL_PATH}")
+        self.log(f"Save file to \t\t\t: {SAVE_PATH}\n")
+        self.log(f"Found {len(self.video_list)} videos and {len(self.caption_list)} subtitle files from environment.\n")
+        
         
     def read(self, save = True, over_write = False):
         self.log(f"Start reading process...(Total Task number:{self.num_video})\n")
@@ -65,7 +76,11 @@ class ExtractionPipeline():
                 task_cnt += 1 
             self.log(f"===")
         self.log(f"Finish job with {task_cnt} file generated.")
-        self.gen_log()
+        with open("log.txt","w") as fp:
+            fp.write(self.logging)
+            fp.close()
+            
+
     def read_once(self,video_name, save = True, over_write = False):
         self.log(f'{video_name}:')
         if not over_write:
@@ -89,6 +104,8 @@ class ExtractionPipeline():
         
         feature = self.frame_to_feature(frames)
         
+        self.get_crtical_time(feature,captions,video_info)
+        
         file = {
             'video_info':video_info,
             'captions':captions,
@@ -107,7 +124,8 @@ class ExtractionPipeline():
             self.log("File not saved.")
         
         return file
-    
+
+
     def read_from_saved_binary_file(self, file_name):
         file_name = file_name[:-4]+'.bin'
         filepath = os.path.join(SAVE_PATH,file_name)
@@ -121,36 +139,28 @@ class ExtractionPipeline():
         self.log(f'Captions: {len(file["captions"])}')
         
         return file
-    
-    def frame_to_feature(self, frames):
-        # TODO: put code here to do feature embedding extraction
-        feature = frames
-        # ===========================
-        
-        return feature
+
     
     def process_video_name(self,video_name):
         vlist = video_name.split("_",3)
         # remove '.mp4'
         vlist[3] = vlist[3][:-4]
-        critical_time = vlist[3].split("_to_")
-        start_time, start_frame     = critical_time[0].split(".")
-        end_time,   end_frame       = critical_time[1].split(".")
+        # critical_time = vlist[3].split("_to_")
+        # start_time, start_frame     = critical_time[0].split(".")
+        # end_time,   end_frame       = critical_time[1].split(".")
         info = {
             'video_name':video_name,
             'video_id'  :vlist[0],
             'video_type':vlist[1],
             'video_info':vlist[2],
-            'start_time':start_time,
-            'start_frame':start_frame,
-            'end_time':end_time,
-            'end_frame':end_frame,
-            'fps':-1,
+            'fps':30,
+            'factor':1,
             'tripping_feature_index':[],
             'tripping_caption_index':[],
             }
         return info
        
+        
     def retrieve_captions(self,video_info, save = False):
         lines = []
         for file in self.caption_list:
@@ -162,12 +172,15 @@ class ExtractionPipeline():
         self.log(f'Get {len(lines)} lines of subtitles.')
         return lines
      
+        
     def retrieve_frames(self,video_info, save = False):
         frames = {}
         video_path = os.path.join(VIDEO_SOURCE_PATH, video_info['video_name'])
         vid_cap = cv2.VideoCapture(video_path)
-        video_info['fps'] = vid_cap.get(cv2.CAP_PROP_FPS)
+        video_info['fps'] = round(vid_cap.get(cv2.CAP_PROP_FPS))
         factor = round(video_info['fps']/self.extracted_fps)
+        factor = 1 if factor == 0 else factor
+        video_info['factor'] = factor
     
         cur_frame = 0
         img_cnt = 0
@@ -193,19 +206,42 @@ class ExtractionPipeline():
         self.log(f"Packed {len(packed_frames)} packs from extracted {img_cnt} frames from total {cur_frame} frames.")
         return packed_frames
 
+
     def log(self, string):
         self.logging += string +'\n'
         if not self.suppress_log:
             print(string)
-            
-    def gen_log(self):
-        with open("log.txt","w") as fp:
-            fp.write(self.logging)
-            fp.close()
         
+    
+    def get_crtical_time(self, feature, captions, video_info):
+        events = self.label[self.label["video"]==video_info["video_name"]]
+        c_feature = []
+        c_caption = []
+        for event in events.iterrows():
+            start_pack = math.floor(event[1].get('beg_frame')/video_info['factor']/self.frame_per_package)
+            end_pack = math.ceil(event[1].get('end_frame')/video_info['factor']/self.frame_per_package)
+            for i in range(start_pack, end_pack+1):
+                c_feature.append(i)
+            for cap in captions:
+                start_time = cap.start.total_seconds()
+                end_time = cap.end.total_seconds()
+                if not (end_time < event[1].get('beg_ts') or start_time > event[1].get('end_ts')):
+                    c_caption.append(cap.index)
+                    
+        video_info['tripping_feature_index'] = list(set(c_feature))
+        video_info['tripping_caption_index'] = list(set(c_caption))
+        
+    def frame_to_feature(self, frames):
+        # TODO: put code here to do feature embedding extraction
+        feature = frames
+        # ===========================
+        
+        return feature
+            
+
 if __name__ == '__main__':
     
-    pipe = ExtractionPipeline(num_video = -1)
+    pipe = ExtractionPipeline(num_video = -1,suppress_log=False)
     pipe.read()
-    #file = pipe.read_once(VID_1, over_write=True)
+    #file = pipe.read_once(VID_10, over_write=True)
     #file_2 = pipe.read_from_saved_binary_file(VID_1)
