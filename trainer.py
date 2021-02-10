68128c575645e49f39d8000c7c3c9bc83e58b8e5
Catosine <pengnan.fan@mail.mcgill.ca>
2021-02-10 01:42:55 -0500

    1

--- trainer.py
+++ trainer.py
@@ -61,7 +61,7 @@ def parse_args():
     parser.add_argument('--text_mapping_layers', type=str, default='0-2048', help='text fully connected layers for common space learning. (default: 0-2048)')
     parser.add_argument('--visual_mapping_layers', type=str, default='0-2048', help='visual fully connected layers  for common space learning. (default: 0-2048)')
     # loss
-    parser.add_argument('--loss_fun', type=str, default='cont', choices=['mrl','cont'], help='loss function')
+    parser.add_argument('--loss_fun', type=str, default='mrl', choices=['mrl','cont'], help='loss function')
     parser.add_argument('--margin', type=float, default=0.2, help='rank loss margin')
     parser.add_argument('--direction', type=str, default='all', help='retrieval direction (all|t2i|i2t)')
     parser.add_argument('--max_violation', action='store_true', help='use max instead of sum in the rank loss')
