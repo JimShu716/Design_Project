5d719afc2a718ff0a19153c2611fb1ca58c023a9
JimShu716 <shuhao716@outlook.com>
2021-02-24 01:39:28 -0500

    pytroch issue for 2.7

--- data_process/Tempuckey/data_provider.py
+++ data_process/Tempuckey/data_provider.py
@@ -32,7 +32,7 @@ class TempuckeyDataSet(data.Dataset):
     def __getitem__(self, index):
         file_path = os.path.join(self.read_path, self.file_pool[index])
         f= open(file_path, 'rb')
-        file = pickle.load(f,fix_imports=True)
+        file = pickle.load(f,encoding="bytes")
         f.close()
         video = (file['feature'], file['video_info']['patch_length'])
         caption = (file['captions'])
